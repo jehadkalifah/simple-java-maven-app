3b7e3a9efcd9836ad69391f3a48cca2c44a0d95b
jehadkalifah <39997755+jehadkalifah@users.noreply.github.com>
2025-11-20 16:02:36 +0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,10 @@ public class App
     private final String message = "Hello World!";
 
     public App() {}
+    public App() {}
+    public App() {}
+    public App() {}
+    public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
